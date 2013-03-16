b021292bcfda75bce52ba27fcb4f3fa961f69c04
snit.ram@gmail.com <snit.ram@gmail.com>
2013-03-16 17:52:14 -0300

    Migrating code to github

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='TimeSlice',
+    version='1.2',
+    author='Rafael Marques Martins',
+    author_email='snit.ram@gmail.com',
+    packages=['TimeSlice',],
+    url='http://github.com/snit-ram/timeslice'
+)
